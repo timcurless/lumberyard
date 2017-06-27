1b3eb7894d890d6cc2ac7cfd78c94a0cc2d1d268
Tim Curless <tim.curless@gmail.com>
2017-06-27 12:35:56 -0500

    Cassandra scaffolding done

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
+/*
+ccm create -v 3.1.1 lumberyard -n 1 -s
+echo "use lumberyard; CREATE TABLE pipelines ( id UUID, name text, description text, PRIMARY KEY (id));" | cqlsh --version 3.4.2
+echo "use lumberyard; CREATE TABLE stages ( id UUID, pipeline_id UUID, name text, description text, type text, version int, payload text, PRIMARY KEY (id));" | cqlsh --version 3.4.2
+echo "use lumberyard; create index on stages (pipeline_id);" | cqlsh --version 3.4.2
+*/
+
 import (
 	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/timcurless/lumberyard/Cassandra"
 )
 
 type heartbeatResponse struct {
@@ -14,6 +22,9 @@ type heartbeatResponse struct {
 }
 
 func main() {
+	CassandraSession := Cassandra.Session
+	defer CassandraSession.Close()
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", heartbeat)
 
